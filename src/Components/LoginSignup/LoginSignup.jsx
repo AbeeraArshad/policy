94c24e46bf838aaf9a105d6eb211ce9f92249446
exit <abeeraarshad2019@gmail.com>
2024-01-11 05:46:24 +0500

    API integration done

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -3,72 +3,139 @@ import "./LoginSignup.css";
 import user_icon from "../Assets/person.png";
 import email_icon from "../Assets/email.png";
 import password_icon from "../Assets/password.png";
+import swal from "sweetalert";
+import axios from "axios";
+// import { act } from "react-dom/test-utils";
 
 const LoginSignup = () => {
-  const [action, setAction] = useState("Login");
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-    confirmpassword:"",
-    
-  });
-  const [forgotPassword, setForgotPassword] = useState(false);
+  const [action, setAction] = useState("Sign Up");
+  // const [formData, setFormData] = useState({
+  //   firstname: "",
+  //   lastname: "",
+  //   name: "",
+  //   email: "",
+  //   password: "",
+  //   confirmpassword: "",
+  // });
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm_password, setConfirmPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [forgetPassword, setForgetPassword] = useState(false);
 
-  const handleChange = (field, value) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      [field]: value,
-    }));
+  const handleSignUp = () => {
+    setAction("Sign Up");
   };
+  const handleSignIn = () => {
+    setAction("Sign In");
+  };
+  const handleSignUpClick = async () => {
+    try {
+      // give api path for calling signUp api on get('/api/signup)
+      const response = await axios.post("http://localhost:5000/api/register", {
+        first_name,
+        last_name,
+        username,
+        email,
+        password,
+        confirm_password,
+      });
 
-  const handleAction = () => {
-    if (action === "Login") {
-      console.log("Logging in with:", formData);
-    } else if (action === "Sign Up") {
-      console.log("Signing up with:", formData);
-    } else {
-      console.log("Forgot Password for:", formData.email);
-
-      setFormData({ name: "", email: "", password: "", confirmpassword:""});
+      console.log(response.data);
+      if (response.status === 201) {
+        // setIsLoggedIn(true);
+        sessionStorage.clear();
+        sessionStorage.setItem("firstname", first_name);
+        sessionStorage.setItem("lastname", last_name);
+        sessionStorage.setItem("username", username);
+        sessionStorage.setItem("email", email);
+        sessionStorage.setItem("password", password);
+        sessionStorage.setItem("confirmpassword", confirm_password);
+        // navigation('/home');
+        swal("User Created Successfully!");
+      } else {
+        swal("Sign-Up failed... Please try again.");
+      }
+    } catch (error) {
+      console.log("Error:", error);
     }
-
-    setFormData({ name: "", email: "", password: "",confirmpassword:""});
   };
+
+  const handleSignInClick = async () => {
+    try {
+      const response = await axios.post("http://localhost:5000/api/login", {
+        username,
+        password,
+      });
+      if (response.status === 200) {
+        setIsLoggedIn(true);
+        // sessionStorage.clear();
+        sessionStorage.setItem("username", username);
+        sessionStorage.setItem("password", password);
+        // navigation('/home');
+        swal("Successfully Logged In")
+      } else {
+        swal("Please sign in with correct credentials.");
+      }
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  // const handleChange = (field, value) => {
+  //   setFormData((prevData) => ({
+  //     ...prevData,
+  //     [field]: value,
+  //   }));
+  // };
+
+  // const handleAction = () => {
+  //   if (action === "Login") {
+  //     console.log("Logging in with:", formData);
+  //   } else if (action === "Sign Up") {
+  //     console.log("Signing up with:", formData);
+  //   } else {
+  //     console.log("Forget Password for:", formData.email);
+
+  //     // setFormData({ firstname:"", lastname:"",name: "", email: "", password: "", confirmpassword:""});
+  //   }
+
+  //   // setFormData({ firstname:"", lastname:"", name: "", email: "", password: "",confirmpassword:""});
+  // };
 
   const dynamicFields =
-    action === "Login" || action === "Forgot Password" ? null : (
-<div>
-  <div className="inputsmall" >
-     
-      <img src={user_icon} alt="" />
-      <input
-        type="text"
-        placeholder="Firstname"
-        value={formData.name}
-        onChange={(e) => handleChange("name", e.target.value)}
-      />
+    action === "Sign In" || action === "Forget Password" ? null : (
+      <div>
+        <div className="inputsmall">
+          <img src={user_icon} alt="" />
+          <input
+            type="text"
+            placeholder="Firstname"
+            onChange={(e) => setFirstName(e.target.value)}
+            value={first_name}
+          />
 
-   
-      <img src={user_icon} alt="" />
-      <input
-        type="text"
-        placeholder="Lastname"
-        value={formData.name}
-        onChange={(e) => handleChange("name", e.target.value)}
-      />
-  
-    </div>
-      
-      <div className="input">
-        <img src={user_icon} alt="" />
-        <input
-          type="text"
-          placeholder="Username"
-          value={formData.name}
-          onChange={(e) => handleChange("name", e.target.value)}
-        />
-      </div>
+          <img src={user_icon} alt="" />
+          <input
+            type="text"
+            placeholder="Lastname"
+            onChange={(e) => setLastName(e.target.value)}
+            value={last_name}
+          />
+        </div>
+
+        <div className="input">
+          <img src={user_icon} alt="" />
+          <input
+            type="text"
+            placeholder="Username"
+            onChange={(e) => setUserName(e.target.value)}
+            value={username}
+          />
+        </div>
       </div>
     );
 
@@ -86,8 +153,8 @@ const LoginSignup = () => {
           <input
             type="email"
             placeholder="Email"
-            value={formData.email}
-            onChange={(e) => handleChange("email", e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
         </div>
         <div className="input">
@@ -95,26 +162,22 @@ const LoginSignup = () => {
           <input
             type="password"
             placeholder="Password"
-            value={formData.password}
-            onChange={(e) => handleChange("password", e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
           />
         </div>
-        
-        {action === "Login" ? (
-          <div></div>
-        ): (
-        <div className="input">
-        <img src={password_icon} alt="" />
-        <input
+        {
+          action === "Sign In" ? <div></div> : 
+          <div className="input">
+          <img src={password_icon} alt="" />
+          <input
             type="password"
-            placeholder="Confirm Password"
-            value={formData.password}
-            onChange={(e) => handleChange("confirmpassword", e.target.value)}
+            placeholder="ConfirmPassword"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            value={confirm_password}
           />
         </div>
-        )}
-
-
+        }       
       </div>
       {/* {(action === 'Sign Up' || action === 'Login') && (
         <div className='forgot-password' onClick={() => setAction('Forgot Password')}>
@@ -122,21 +185,21 @@ const LoginSignup = () => {
         </div>
       )} */}
 
-      {action === "Sign Up" ? (
+      {/* {action === "Sign Up" || forgetPassword ? (
         <div></div>
       ) : (
         <div
           className="forgot-password"
-          onClick={() => setAction("Forgot Password")}
+          onClick={() => setAction("Forget Password")}
         >
-          Forgot Password?<span>Click Here..</span>
+          Forget Password? <span> Click Here..</span>
         </div>
-      )}
-      <div className="submit-container">
-        <div className="submit" onClick={handleAction}>
-          {action === "Forgot Password" ? "Reset" : action}
+      )} */}
+      {/* <div className="submit-container">
+        <div className="submit">
+          {action === "Forget Password" ? "Reset" : action}
         </div>
-      </div>
+      </div> */}
 
       {/* <div className='submit-container'>
         <div className='submit' onClick={handleAction}>
@@ -144,11 +207,45 @@ const LoginSignup = () => {
         </div>
       </div> */}
       <div className="switch-action">
-        <span
-          onClick={() => setAction(action === "Login" ? "Sign Up" : "Login")}
-        >
-          {action === "Login" ? "Sign Up" : "Login"}
-        </span>
+        {/* <div className="switch">
+          <button
+            type="submit"
+            className="submit"
+            onClick={
+              action === "Sign Up" ? handleSignInClick : handleSignUpClick
+            }
+          >
+            {action}
+            {console.log("action:",action)}
+          </button>
+          {action === "Sign In" ? (
+            <div></div>
+          ) : (
+            <div className="account">
+              Click here to create account{" "}
+              <span onClick={handleSignUp}>Sign In</span>
+            </div>
+          )}
+        </div> */}
+        <div className="switch">
+          <button
+            type="submit"
+            className="submit"
+            onClick={
+              action === "Sign Up" ? handleSignUpClick : handleSignInClick  
+            }
+          >
+            {action}
+            {console.log("action:", action)}
+          </button>
+          {action === "Sign In" ? (
+            <div></div>
+          ) : (
+            <div className="account">
+              Don't have an account? <span onClick={handleSignIn}>Sign In</span>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
